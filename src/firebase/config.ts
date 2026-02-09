4a128c40831e55c3370c9753954f57261f042d48
אלי לאופר <elilaufer@gmail.com>
2026-02-09 19:48:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8618186014-74886",
+  "appId": "1:984337619270:web:7a79a2cbea09bec0d2ad47",
+  "apiKey": "AIzaSyDFgIbRfLKJvNtsU0c7uTBD-AtfuSoyJVY",
+  "authDomain": "studio-8618186014-74886.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "984337619270"
+};
